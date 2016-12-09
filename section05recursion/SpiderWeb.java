a9120975e785a867e077e37b7ca5f9bd82466355
91076269 <91076269@mvusd.net>
2016-12-09 13:27:40 -0800

    AdLibsQuiz_SuperTurtle

--- section05recursion/SpiderWeb.java
+++ section05recursion/SpiderWeb.java
@@ -1,38 +1,46 @@
 package org.teachingkidsprogramming.section05recursion;
 
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.Turtle.Animals;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
 
 public class SpiderWeb
 {
   public static void main(String[] args)
   {
     Tortoise.show();
-    //    Make the tortoise go as fast as possible --#6
-    //    Change the width of the line the tortoise draws to 1 pixel --#12
-    //    Change the Tortoise to a Spider --#14
-    //    Change the pen color of the line the tortoise draws to red --#13.1
-    //    Set the color of the background window to black (HINT: use the Tortoise to get the background window) --#13.2
-    //    The current zoom is 1.1 --#8.2
-    //    Do the following 10 times --#10.1
-    //        weaveOneLayer (recipe below) --#9.1
-    //
-    //        ------------- Recipe for weaveOneLayer --#9.2
-    //        Do the following 6 times --#5.1
-    //            drawTriangle (recipe below) --#4.1
-    //
-    for (int i = 0; i < 3; i++)
+    Tortoise.setSpeed(10);
+    Tortoise.setPenWidth(1);
+    Tortoise.setAnimal(Animals.Spider);
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.getBackgroundWindow().setBackground(PenColors.Grays.Black);
+    float lengthOfLine = 10.5f;
+    float zoom = 1.1f;
+    for (int i = 0; i < 10; i++)
     {
-      int line = (int) 10.5;
+      lengthOfLine = weaveOneLayer(lengthOfLine, zoom);
+      zoom = zoom * 1.3f;
+    }
+  }
+  private static float weaveOneLayer(float lengthOfLine, float zoom)
+  {
+    for (int i = 0; i < 6; i++)
+    {
+      //
+      drawTriangle();
+      //
+      Tortoise.turn(360 / 6);
+      lengthOfLine = lengthOfLine + zoom;
+    }
+    return lengthOfLine;
+  }
+  private static void drawTriangle()
+  {
+    for (int i1 = 0; i1 < 3; i1++)
+    {
+      double line = 10.5;
       Tortoise.move(line);
       Tortoise.turn(360 / 3);
     }
-    //
-    //            Turn the tortoise 1/6th of 360 degrees to the right --#7
-    //            Increase the current length (of the line) by the current zoom --#8.1
-    //        Repeat --#5.2
-    //        ------------- End of weaveOneLayer recipe --#9.3
-    //
-    //        Change the current zoom so it is multiplied by 1.3 --#11
-    //    Repeat --#10.2
   }
 }
--- section07objects/SuperTurtles.java
+++ section07objects/SuperTurtles.java
@@ -1,6 +1,7 @@
 package org.teachingkidsprogramming.section07objects;
 
 import org.teachingextensions.WindowUtils.MultiTurtleWindow;
+import org.teachingextensions.logo.Turtle;
 
 public class SuperTurtles
 {
@@ -16,23 +17,40 @@ public class SuperTurtles
   private void showSomeTurtles()
   {
     //  makeSpeedyTurtle (recipe below) --#2.0
-    //  ------------- Recipe for makeSpeedyTurtle --#1.0
-    //        Create a new speedyTurtle instance  
-    //        Add your speedyTurtle to your MultiTurtleWindow
-    //        Set the speed of your speedyTurtle to the fastest possible    
-    //        Have your speedyTurtle draw a triangle with 100 pixel sides
-    //  ------------- End of makeSpeedyTurtle --#1.1 
+    makeSpeedyTurtle();
     //  makeSlowTurtle (recipe below) --#4.0
     //  ------------- Recipe for makeSlowTurtle --#3.0
-    //        Create a new slowTurtle instance 
-    //        Add your slowTurtle to your MultiTurtleWindow
-    //        Have your slowTurtle draw a upside down triangle with 50 pixel sides
+    makeSlowTurtle();
     //  ------------- End of makeSlowTurtle --#3.1
     //  makeCrazyTurtle (recipe below) --#6.0
+    makeCrazyTurtle();
+    //  ------------- End of makeCrazyTurtle --#5.2
+  }
+  private void makeCrazyTurtle()
+  {
     //  ------------- Recipe for makeCrazyTurtle --#5.1
     //        Create a new crazyTurtle instance  
+    Turtle crazyTurtle = new Turtle();
     //        Add your crazyTurtle to your MultiTurtleWindow
+    mtw.addAndShowTurtle(crazyTurtle);
     //        Have your crazyTurtle draw a 55 pixel long lightning bolt
-    //  ------------- End of makeCrazyTurtle --#5.2
+    crazyTurtle.drawLightning(55);
+  }
+  private void makeSlowTurtle()
+  {
+    //        Create a new slowTurtle instance 
+    Turtle slowTurtle = new Turtle();
+    mtw.addAndShowTurtle(slowTurtle);
+    //        Have your slowTurtle draw a upside down triangle with 50 pixel sides
+    slowTurtle.drawTriangle(-50);
+  }
+  private void makeSpeedyTurtle()
+  {
+    //  ------------- Recipe for makeSpeedyTurtle --#1.0
+    Turtle speedyTurtle = new Turtle();
+    mtw.addAndShowTurtle(speedyTurtle);
+    speedyTurtle.setSpeed(8);
+    speedyTurtle.drawTriangle(100);
+    //  ------------- End of makeSpeedyTurtle --#1.1 
   }
 }
